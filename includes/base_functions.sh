d8b7f78f57cb243539468f4d8a9468b5c12a001f
Kobunix <kobunix@gmail.com>
2022-11-28 04:13:57 +0100

    Adding message function

--- includes/base_functions.sh
+++ includes/base_functions.sh
@@ -4,6 +4,14 @@ export SCRIPT_AUTHOR="Kobunix"
 export GLOBSCRIPT_VERSION="0.0.3"
 
 
+
+ShowMessage ()
+{
+	echo "${RED}[ ${WHITE}$1 ${RED}]\t ${WHITE}$2 ${NC}"
+	sleep 1
+}
+
+
 Header () {
 	echo "${Green}===========================================================================================${NC}"
 	echo "${Green}=     ${WHITE}########   #######   #######  ######## ##      ##  #######  ########  ##    ##      ${Green}=${NC}"
